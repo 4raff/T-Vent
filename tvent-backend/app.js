c80c54679e3361958f97fb9859602785ba0ce302
naren <tegar.narendra@gmail.com>
2025-11-18 16:53:06 +0700

    fixing register and login

--- tvent-backend/app.js
+++ tvent-backend/app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = 3000
-
+const authRoutes = require('./src/routes/authRoutes');
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.json({ 
